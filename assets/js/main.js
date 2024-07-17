126743346c14744c5f7a9d109fb4191e7892a397
unknown <maximilianoacri1@gmail.com>
2024-07-17 16:46:54 -0300

    todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -310,3 +310,25 @@
   }
 
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  // Mostrar/ocultar el botón al hacer scroll
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 300) {
+          backToTopButton.classList.add('active');
+      } else {
+          backToTopButton.classList.remove('active');
+      }
+  });
+
+  // Desplazamiento suave hacia la parte superior al hacer clic
+  backToTopButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  });
+});
